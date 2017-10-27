4f2648759cf0bafed6a850ad478057ae85f67485
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-10-27 11:57:08 +0300

    Remove old modules

--- src/Food.js
+++
@@ -1,8 +0,0 @@
-import Element from './Element';
-
-export default class Food extends Element {
-    constructor({ game, container, width, height, x, y, color }) {
-        super({ game, container, width, height, x, y, direction, color });
-        
-    }
-}
